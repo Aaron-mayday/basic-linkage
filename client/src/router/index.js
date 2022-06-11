a644ecd2d5e34dce31e64f401c53760a33c28b8f
Aaron-mayday <43854326+Aaron-mayday@users.noreply.github.com>
2022-06-11 21:12:29 +0800

    client

---
+++ client/src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView
+  }
+]
+
+const router = new VueRouter({
+  // mode: 'history',  //history部署到后台，会有额外的配置
+  base: process.env.BASE_URL,
+  routes
+})
+
+router.beforeEach((to,from,next)=>{
+	if(to.path === "/login"){
+		next();
+	}else{
+		let token = localStorage.getItem("token");
+		if(token){
+			next();
+		}else{
+			next("/login")
+		}
+	}
+})
+
+export default router
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="home">
+		<h1>数据:{{msg}}</h1>
+		<button @click="logout">注销</button>
+	</div>
+</template>
+
+<script>
+	import request from "../../utils/request.js"
+	export default {
+		data(){
+			return{
+				msg:""
+			}
+		},
+		methods: {
+			logout(){
+				localStorage.setItem("token","");
+				location.reload();
+			},
+			getMsg(){
+				// let token = localStorage.getItem("token");
+				request.get(`/hello`).then((res)=>{
+					this.msg = res.data.data[0].msg
+				})
+			}
+			
+		},
+		created(){
+			this.getMsg();
+		}
+	}
+</script>
